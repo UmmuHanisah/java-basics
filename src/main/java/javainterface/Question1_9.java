b1e4374d62281493c175876f544cd20da38cbec1
UmmuHanisah <ummuhanisah0625@gmail.com>
2020-09-07 10:44:36 +0800

    Added question 1 & 9

---
+++ src/main/java/javainterface/Question1_9.java
@@ -0,0 +1,14 @@
+package javainterface;
+
+/**
+ *
+ * @author uha
+ */
+public class Question1_9 {
+    // Question 1: For every interface written in a java file, .class file will be generated after compilation? True or False?
+    // Answer: True. For every interface written in a java file, .class file will be generated after compilation.
+    
+    // Question 9: Can interfaces have constructors?
+    // No. Interfaces can’t have constructors.
+    
+}
