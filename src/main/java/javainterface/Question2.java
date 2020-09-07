ed42426cf39f16ba19e11cf19dc3130cea2493e3
UmmuHanisah <ummuhanisah0625@gmail.com>
2020-09-07 10:07:41 +0800

    Initial commit

---
+++ src/main/java/javainterface/Question2.java
@@ -0,0 +1,19 @@
+package javainterface;
+
+/**
+ *
+ * @author uha
+ * @source https://javaconceptoftheday.com/java-practice-coding-questions-on-interfaces/
+ * 
+ * @question 2) Can you identify the error in the below code?
+ * @answer Illegal modifier for field i. Only public, static and final are allowed.
+ */
+
+interface Anis
+{
+//    private int i;
+}
+
+public class Question2 {
+    
+}
