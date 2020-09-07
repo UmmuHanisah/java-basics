cb3f3ddec43a66238e2aa49428647304095e99ff
UmmuHanisah <ummuhanisah0625@gmail.com>
2020-09-07 11:01:24 +0800

    Added Question 10

---
+++ src/main/java/javainterface/Question10.java
@@ -0,0 +1,17 @@
+package javainterface;
+
+/**
+ *
+ * @author uha
+ */
+public class Question10 {
+    
+}
+
+class Apricot implements Banana {
+    public int methodB(int i);
+}
+
+interface Banana {
+    int methodB(int i);
+}
